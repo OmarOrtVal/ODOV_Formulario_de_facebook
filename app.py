27623b4ae8cc4efe13c3a13c42a4999288d409aa
OmarOrtVal <23308060610674@cetis61.edu.mx>
2025-10-01 19:04:56 -0700

    intento 1

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<a href="/static/formulario.html">Ir al Formulario</a>'
+
+@app.route('/registrar/<nombre>/<apellido>/<contacto>/<genero>/<fecha>/<contrasena>')
+def registrar(nombre, apellido, contacto, genero, fecha, contrasena):
+    print("--- Nuevo Registro ---")
+    print(f"Nombre: {nombre} {apellido}")
+    print(f"Contacto: {contacto}")
+    print(f"Fecha de Nacimiento: {fecha}")
+    print(f"Género: {genero}")
+    print(f"Contraseña: {contrasena}")
+    print("----------------------")
+
+    return f"""
+    <h2>¡Registro exitoso!</h2>
+    <p><b>Nombre:</b> {nombre} {apellido}</p>
+    <p><b>Contacto:</b> {contacto}</p>
+    <p><b>Fecha de Nacimiento:</b> {fecha}</p>
+    <p><b>Género:</b> {genero}</p>
+    <p><b>Contraseña:</b> {contrasena}</p>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
